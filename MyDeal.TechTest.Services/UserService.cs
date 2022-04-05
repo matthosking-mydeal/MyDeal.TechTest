8a2d63a6f83710c9661993ea5bd779a67410d71d
Matt Hosking <matthew.hosking@mydeal.com.au>
2022-04-05 19:18:03 +1000

    Add project files.

---
+++ MyDeal.TechTest.Services/UserService.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace MyDeal.TechTest.Services
+{
+    public static class UserService
+    {
+        public static UserData GetUserDetails(string userId)
+        {
+            var response = WebRequest.Create("https://reqres.in/api/users/" + userId).GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string json = reader.ReadToEnd();
+            return JsonConvert.DeserializeObject<UserData>(json);
+        }
+    }
+}
